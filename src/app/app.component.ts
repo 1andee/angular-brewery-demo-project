4cfff01abd119579b0dfee48c992ca6f1d6977fe
Ian Duke <ian@koho.ca>
2022-10-03 13:16:01 -0400

    Fetch random brewery on page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filter, tap } from 'rxjs/operators';
+import { OnGetRandomBreweryAction } from './brewery/store/brewery.actions';
+import { selectRandomBrewery } from './brewery/store/brewery.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'super-cool-demo-angular-project';
+  public randomBrewery$ = this.store
+    .select(selectRandomBrewery)
+    .pipe(filter((brewery) => !!brewery));
+
+  constructor(private store: Store) {}
+
+  public ngOnInit(): void {
+    this.store.dispatch(OnGetRandomBreweryAction.Start());
+  }
 }
