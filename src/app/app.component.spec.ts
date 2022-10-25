22d1fe2a50458cfb9b0c28a2a3f8120172c475dd
Ian Duke <ian@koho.ca>
2022-10-25 14:12:06 -0400

    Update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,17 +18,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'super-cool-demo-angular-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('super-cool-demo-angular-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('super-cool-demo-angular-project app is running!');
-  });
 });
